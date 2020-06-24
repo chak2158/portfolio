953a7cdaa423e3503790b72dea2d76f97250c41d
chak2158 <chak2158@naver.com>
2020-06-24 16:01:11 +0900

    add project

---
+++ goupangProject/src/main/java/com/goupang/www/controller/security/SecurityController.java
@@ -0,0 +1,12 @@
+package com.goupang.www.controller.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SecurityController {
+	@RequestMapping("/security/securityTest")
+	public void list()throws Exception {
+		
+	}
+}
